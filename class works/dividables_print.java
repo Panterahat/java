0ad8bbbd2c961a3b5a49657c0977c49024375bde
Panterahat <raverahat@gmail.com>
2025-08-25 22:04:31 +0600

    hello

--- class works/dividables_print.java
+++ class works/dividables_print.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+
 public class dividables_print {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("enter the number to divide with : ");
         int n = input.nextInt();
         System.out.print("enter the starting range : ");
@@ -9,11 +10,11 @@ public class dividables_print {
         System.out.print("enter the ending range : ");
         int end = input.nextInt();
         System.out.println("the numbers between " + start + " and " + end + " that are divisible by " + n + " are : ");
-        for(int i=start; i<=end;i++){
+        for (int i = start; i <= end; i++) {
 
-            if(i%n == 0){
-                System.out.println(i   );
-            }            
+            if (i % n == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
